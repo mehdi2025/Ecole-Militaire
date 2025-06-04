f37994cabae1e8f15cc2cf919961c52e647b7395
mehdi2025 <156846568+mehdi2025@users.noreply.github.com>
2025-06-04 22:38:30 +0100

    Update urls.py

--- CollegeERP/urls.py
+++ CollegeERP/urls.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from mozilla_django_oidc import views as oidc_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
     path('info/', include('info.urls')),
     path('api/', include('apis.urls')),
-    path('accounts/login/',
-         auth_views.LoginView.as_view(template_name='info/login.html'), name='login'),
-    path('accounts/logout/',
-         auth_views.LogoutView.as_view(template_name='info/logout.html'), name='logout'),
+    # Replace Django auth views with OIDC views
+    path('accounts/login/', oidc_views.OIDCAuthenticationRequestView.as_view(), name='login'),
+    path('accounts/logout/', oidc_views.OIDCLogoutView.as_view(), name='logout'),
+    
+    # Add OIDC URLs
+    path('oidc/', include('mozilla_django_oidc.urls')),
 ]
+
